b4bf97f021f73845802e9dbe9882bb9457afbcb6
Frevens <frevensgames@gmail.com>
2026-07-30 16:56:46 +0000

    CH1: Servers

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Frevens/chirpy
+
+go 1.26.4
--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
+func readiness(w http.ResponseWriter, r *http.Request) {
+	// Set the Content-Type header
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 
+	// Write the status code (e.g., 200 OK)
+	w.WriteHeader(http.StatusOK)
+
+	// Write the response body
+	w.Write([]byte("OK"))
+}
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
 
+	fs := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app/", fs))
+	mux.HandleFunc("/healthz/", readiness)
+
+
+	
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Fatal(server.ListenAndServe())
 }
