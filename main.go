acf3bed76a03d36033e9d8175a43246d2658bcfe
Frevens <frevensgames@gmail.com>
2026-08-04 17:20:24 +0000

    CH4: JSON

--- main.go
+++ main.go
@@ -1,41 +1,54 @@
 package main
 
 import (
-	"log"
+	"encoding/json"
+	"strings"
 	"fmt"
+	"log"
 	"net/http"
 	"sync/atomic"
-
-	
 )
+
 type apiConfig struct {
-fileserverHits atomic.Int32
+	fileserverHits atomic.Int32
 }
+
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Atomically increment the counter by 1
 		cfg.fileserverHits.Add(1)
-		
-		// Call the next handler in the chain
 		next.ServeHTTP(w, r)
 	})
 }
 
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
+type cleanResponse struct {
+	CleanedBody string `json:"cleaned_body"`
+}
+
+type parameters struct {
+	Body string `json:"body"`
+}
+
+var listOfBannedWords = []string{
+	"kerfuffle", 
+	"sharbert", 
+	"fornax",
+}
+
 func readiness(w http.ResponseWriter, r *http.Request) {
-	// Set the Content-Type header
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 
-	// Write the status code (e.g., 200 OK)
 	w.WriteHeader(http.StatusOK)
 
-	// Write the response body
 	w.Write([]byte("OK"))
 }
+
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	// Load the current value atomically
 	hits := cfg.fileserverHits.Load()
-		
-	// Write the formatted response
+
 	htmlContent := fmt.Sprintf(
 		`<html>
 	<body>
@@ -46,30 +59,96 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 		hits,
 	)
 
-	// Set the content type to html text 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, htmlContent)
-}   
+}
+
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-	// Atomically reset the counter to 0
 	cfg.fileserverHits.Store(0)
-	
-	// Set content type to plain text
+
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	
-	// Confirm the reset in the response
+
 	fmt.Fprintf(w, "Hits: 0\n")
 }
+
+func (cfg *apiConfig) handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+
+	var requestData parameters
+
+	if err := decoder.Decode(&requestData); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Failed to decode JSON")
+		return
+	}
+
+	if len(requestData.Body) > 140 {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	cleanedBody := cleanChirp(requestData.Body)
+
+	respondWithJSON(w, http.StatusOK, cleanResponse{
+		CleanedBody: cleanedBody,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, status int, payload any) {
+	response, err := json.Marshal(payload)
+
+	if err != nil {
+		jsonError := errorResponse{
+			Error: "Something went wrong",
+		}
+
+		response, _ = json.Marshal(jsonError)
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(response)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(status)
+	w.Write(response)
+}
+
+func respondWithError(w http.ResponseWriter, status int, message string) {
+	respondWithJSON(w, status, errorResponse{
+		Error: message,
+	})
+}
+
+func cleanChirp(body string) string {
+	words := strings.Split(body, " ")
+
+	for i, word := range words {
+		lowerWord := strings.ToLower(word)
+
+		for _, banned := range listOfBannedWords {
+			if lowerWord == banned {
+				words[i] = "****"
+			}
+		}
+	}
+
+	return strings.Join(words, " ")
+}
+
 func main() {
 	mux := http.NewServeMux()
 	cfg := &apiConfig{}
 
 	fs := http.FileServer(http.Dir("."))
+
 	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", fs)))
+
 	mux.HandleFunc("GET /api/healthz", readiness)
 	mux.HandleFunc("GET /admin/metrics", cfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", cfg.handlerReset)
-	
+	mux.HandleFunc("POST /api/validate_chirp", cfg.handlerValidateChirp)
+
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
